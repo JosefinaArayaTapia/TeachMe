0368b5538155d8463b5f5c13c00e738426856b2c
Josefina Araya <araya.tapia.j@gmail.com>
2016-09-28 15:24:07 -0300

    Creacion de BaseSeeder y TicketSeeder

---
+++ database/seeds/BaseSeeder.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Macbook
+ * Date: 28-09-16
+ * Time: 10:39 AM
+ */
+class BaseSeeder
+{
+
+}
---
+++ database/seeds/TicketTableSeeder.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Macbook
+ * Date: 28-09-16
+ * Time: 3:14 PM
+ */
+class TicketTableSeeder
+{
+
+}
